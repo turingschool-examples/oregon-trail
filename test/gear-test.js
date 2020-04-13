0335a1950c6dd27fb61cdefde591f33fa4d6e895
Will Mitchell <wvmitchell@gmail.com>
2020-04-13 12:58:03 -0600

    add test suite for gear

---
+++ test/gear-test.js
@@ -0,0 +1,41 @@
+var assert = require('chai').assert;
+var Gear = require('../src/Gear');
+
+describe('Gear', function() {
+  it('should have a type', function() {
+    var food = new Gear('food');
+
+    assert.equal(food.type, 'food');
+  })
+
+  it('should be able to have a different type', function() {
+    var ammunition = new Gear('ammunition');
+
+    assert.equal(ammunition.type, 'ammunition');
+  })
+
+  // valid types are food, ammunition, and clothes
+  it('should not be able to have an invalid type', function() {
+    var ammunition = new Gear('ammunition');
+    var food = new Gear('food');
+    var clothes = new Gear('clothes');
+    var invalid = new Gear('mp3 player');
+
+    assert.equal(ammunition.type, 'ammunition');
+    assert.equal(food.type, 'food');
+    assert.equal(clothes.type, 'clothes');
+    assert.equal(invalid.type, undefined);
+  })
+
+  it('should be able to have a quantity', function() {
+    var food = new Gear('food', 10);
+
+    assert.equal(food.quantity, 10);
+  })
+
+  it('should be able to have a different quantity', function() {
+    var food = new Gear('food', 12);
+
+    assert.equal(food.quantity, 12);
+  })
+})
