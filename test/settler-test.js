3fb62a5d3252e51b6d9320cd432131517c00798c
Will Mitchell <wvmitchell@gmail.com>
2020-04-13 13:29:40 -0600

    update tests to expect object in constructor

--- test/settler-test.js
+++ test/settler-test.js
@@ -1,57 +1,57 @@
 var assert = require('chai').assert;
-var Settler = require('../src/Settler');
+var Settler = require('../src/settler.js');
 
 describe('Settler', function() {
   it('should have a name', function() {
-    var traveler = new Settler('Will');
+    var traveler = new Settler({name: 'Will'});
 
     assert.equal(traveler.name, 'Will');
   })
 
   it('should be able to have a different name', function() {
-    var traveler = new Settler('Leta');
+    var traveler = new Settler({name: 'Leta'});
 
     assert.equal(traveler.name, 'Leta');
   })
 
   it('should have an age', function() {
-    var traveler = new Settler('Will', 33);
+    var traveler = new Settler({name: 'Will', age: 33});
 
     assert.equal(traveler.age, 33);
   })
 
   it('should be able to have a different age', function() {
-    var traveler = new Settler('Will', 21);
+    var traveler = new Settler({name: 'Will', age: 21});
 
     assert.equal(traveler.age, 21);
   })
 
   it('should have an unknown nationality by default', function() {
-    var traveler = new Settler('Will', 21);
+    var traveler = new Settler({name: 'Will', age: 21});
 
     assert.equal(traveler.nationality, 'unknown');
   })
 
   it('should be able to have a nationality', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     assert.equal(traveler.nationality, 'English');
   })
 
   it('should be healthy by default', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     assert.equal(traveler.status, 'healthy');
   })
 
   it('should have no ailments by default', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     assert.deepEqual(traveler.ailments, []);
   })
 
   it('should be able to experience distress', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     traveler.experienceDistress('broken arm')
 
@@ -60,7 +60,7 @@ describe('Settler', function() {
   })
 
   it('should be able to become more distressed', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     traveler.experienceDistress('broken arm')
     traveler.experienceDistress('dysentery')
@@ -70,7 +70,7 @@ describe('Settler', function() {
   })
 
   it('should be able to be killed', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     traveler.experienceDistress('broken arm')
     traveler.experienceDistress('snake bite')
@@ -81,7 +81,7 @@ describe('Settler', function() {
   })
 
   it('should not be able to experience distress after death', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     traveler.experienceDistress('broken arm')
     traveler.experienceDistress('snake bite')
@@ -93,7 +93,7 @@ describe('Settler', function() {
   })
 
   it('should be able to be healed', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     traveler.experienceDistress('broken arm')
     traveler.experienceDistress('snake bite')
@@ -104,7 +104,7 @@ describe('Settler', function() {
   })
 
   it('should not be able to be healed if dead', function() {
-    var traveler = new Settler('Will', 21, 'English');
+    var traveler = new Settler({name: 'Will', age: 21, nationality: 'English'});
 
     traveler.experienceDistress('broken arm')
     traveler.experienceDistress('snake bite')
